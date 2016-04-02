1b3c6cda74288adf0200048f23a44e452f5dfc47
Chris Clarke <chrisclarke1977@gmail.com>
2016-04-02 15:07:45 +0000

    test runner problems with require

--- tests.webpack.js
+++ tests.webpack.js
@@ -1,2 +1,16 @@
-var context = require.context('./test', true, /-test\.js$/);
-context.keys().forEach(context);
+var tests = [];
+for (var file in window.__karma__.files) {
+    if (/-test\.js$/.test(file)) {
+        tests.push(file);
+    }
+}
+
+requirejs.config({
+    baseUrl: '/src',
+    paths: {
+    },
+    shim: {
+    },
+    deps: tests,
+    callback: window.__karma__.start
+});
