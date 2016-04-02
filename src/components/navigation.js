0dc059d1f3302ebac4bc2a62b668e93b6697dc49
Chris Clarke <chrisclarke1977@gmail.com>
2016-04-02 14:15:09 +0000

    Adding tests

---
+++ src/components/navigation.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {renderIntoDocument} from 'react-addons-test-utils';
+import { expect} from 'chai';
+import { Navigation } from 'components';
+import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router';
+
+export default {
+    
+};
---
+++ test/navigation.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {renderIntoDocument} from 'react-addons-test-utils';
+import { expect} from 'chai';
+import { Navigation } from 'components';
+import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router';
+
+describe('Navigation', () => {
+    
+  it('should render correctly', () => {
+    return expect(renderer).to.be.ok;
+  });
+});
